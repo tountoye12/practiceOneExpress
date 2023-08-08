aa4b32b3c31cfd2b7241e02b12f43dcc6bd820e5
tountoye12 <mamadouadama609@gmail.com>
2023-08-07 23:18:49 -0500

    second commit

--- app.js
+++ app.js
@@ -2,17 +2,14 @@
 
 const express = require('express');
 
-
-
-
-
 const app = express();
+const path = require("path")
 
 
 
 //Read the parameters from post request
 app.use(express.json());
-app.use(express.urlencoded({urlencoded:false}));
+app.use(express.urlencoded({ extended: false }));
 
 const port = 3000;
 
@@ -21,6 +18,30 @@ const port = 3000;
 app.listen(port, () => console.log(`server is running on port ${port}`))
 
 
+let studentResponses = [];
+// 1. Show the exam.html page when the User hits the landing page('/')
+app.get("/home",
+    (req, res, next) => res.sendFile(path.join(__dirname, "html", "exam.html"))
+);
+
+//2. When User enters the data and clicks on the submit post to the ("/post") url
+
+app.post("/post", (req, res, next) => {
+
+    studentResponses.push(req.body.questionOne)
+    studentResponses.push(req.body.questionTwo)
+    console.log(studentResponses);
+    res.send(JSON.stringify(studentResponses))
+});
+
+
+//3. Display '/preview' page with all the User entered details. 
+
+app.get("/preview", (req, res, next) => {
+    res.send(JSON.stringify(studentResponses));
+});
+
+console.log("Outsit " + studentResponses);
 
 
 
